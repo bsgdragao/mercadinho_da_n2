de0ba5d31f7de7eb782465eac3ec3b5cec10866f
Bruno Guerra <bsgdragao@gmail.com>
2020-04-25 22:24:13 -0300

    Finalizada tela home

--- index.php
+++ index.php
@@ -4,9 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=
     , initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
     <title>Mercadinho da N2</title>
+    
 </head>
 <body>
-    
+    <?php  require_once "menu.php"; ?>
+    <div id="titulo">Mercadinho da N2</div>
+
+ 
 </body>
+<footer id="footer">
+  <p>by: @BrunoG</p>
+</footer>
 </html>
---
+++ menu.php
@@ -0,0 +1,6 @@
+  
+<div id="menu">
+    <a href="index.php"><button id="btHome">Home</button></a>
+    <a href="clsProduto.php"><button id="btCatalog">Catálogo</button></a>
+    <a href="function.php"><button id="btAdmin">Administrativo</button></a>
+</div>
